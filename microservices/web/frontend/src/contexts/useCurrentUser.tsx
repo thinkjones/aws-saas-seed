6a5f4cc3498ee35a69e1a160263749d92af85980
Gene Conroy-Jones <gene@projectdado.com>
2022-11-05 14:38:59 -0700

    Add useCurrentUser Hook to get data

---
+++ microservices/web/frontend/src/contexts/useCurrentUser.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { gql, useQuery } from '@apollo/client';
+
+const GET_CURRENT_USER = gql`
+    query GetCurrentUser {
+        getCurrentUser {
+            id
+            name
+            email
+        }
+    }
+`;
+
+const onlyCapitalLetters = (str: string) => {
+  return str.replace(/[^A-Z]+/g, "");
+}
+
+const useCurrentUser = () => {
+  const { loading, error, data } = useQuery(GET_CURRENT_USER);
+  return {
+    loading,
+    error,
+    ...data?.getCurrentUser,
+    initials: onlyCapitalLetters(data?.getCurrentUser?.name)
+  }
+}
+
+export default useCurrentUser
